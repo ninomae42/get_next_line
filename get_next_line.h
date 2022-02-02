38c3a15088e56c9aabb135e375a83812884e1957
Takuma Shimizu <tashimiz@student.42tokyo.jp>
2022-02-02 23:54:57 +0900

    [Update] Update the prototype declaration in get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -2,7 +2,17 @@
 # define GET_NEXT_LINE_H
 # include <stdlib.h>
 # include <unistd.h>
+# include <stddef.h>
 
 char	*get_next_line(int fd);
+char	*read_and_save(int fd, char *save);
+char	*get_line(char *save);
+char	*trim_save(char *save);
+
+size_t	ft_strlen_s(const char *s);
+char	*ft_strchr_s(const char *s, int c);
+char	*ft_strdup_s(const char *s);
+char	*ft_stpcpy_s(char *dst, const char *src);
+char	*ft_strjoin(const char *s1, const char *s2);
 
 #endif
